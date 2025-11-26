00c7a356912fc0346e2e297daa277a3a702772de
Brian Tarqui <tarquibrian@gmail.com>
2025-11-26 02:26:46 -0400

    feat(repository): agreagar repositorios para Usuarios, Producto y Marca

---
+++ backend/ecoshop/src/main/java/com/ecoshop/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.ecoshop.repository;
+
+import com.ecoshop.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+  boolean existsByEmail(String email);
+}
